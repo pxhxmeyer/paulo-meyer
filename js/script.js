c5b127b0ede8e6334c34182144c6e0c8fa42e1c7
Paulo Meyer <pxhxmeyer@gmail.com>
2026-07-25 12:53:43 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+
+// Tag filter
+document.querySelectorAll('.filter-btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    document.querySelectorAll('.filter-btn').forEach(b => b.classList.remove('active'));
+    btn.classList.add('active');
+    const filter = btn.dataset.filter;
+    document.querySelectorAll('.card').forEach(card => {
+      if(filter === 'all' || card.dataset.category.split(' ').includes(filter)){
+        card.classList.remove('hidden');
+      } else {
+        card.classList.add('hidden');
+      }
+    });
+  });
+});
